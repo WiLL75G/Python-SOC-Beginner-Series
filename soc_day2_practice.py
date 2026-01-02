7addf42807c837c148ece2d71133469e4b7ad1f2
William <wgokahp@gmail.com>
2026-01-02 06:43:05 -0800

    Initial commit

---
+++ soc_day2_practice.py
@@ -0,0 +1,55 @@
+# ------------------------------
+# Beginner Soc Practice o1
+#-------------------------------
+
+# Why I am learning this 
+
+# SOC analysts must clearly see
+# .Alerts 
+# .Logs
+# .Detection results
+
+# Python output helps me visualize security data
+
+# Problem solved: 
+#  Displaying alerts and investigateion messages clearly
+
+
+
+#PYTHON BASICS REMAINDER:
+
+
+
+#print() shows output
+#texts goes inside quotes
+
+#code :
+#input
+
+
+
+
+
+
+print("Unauthorized access attempt detected")
+
+
+
+
+
+
+
+
+
+#outpt :
+# Unauthorized access attempt detected 
+
+
+#SOC Use:
+# .Display alerts 
+# .Show investigation 
+
+
+# Clear Output = Clear Investigation
+
+
